018e262099f118ae91ab8dc9dfa60cd197899ed6
Parham-CHs <parhamjavan.ch@gmail.com>
2025-05-15 04:06:45 +0430

    conf update for email

--- Project/config.py
+++ Project/config.py
@@ -4,6 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 class Config:
+
+
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")  # Fallback if .env not found
+
+    # DataBase    
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email Configuration (Zoho)
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    # MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
+
